563c095451d3cd86d2540a4df6c33510ae44e0f8
Dmitrii Kushchenko <dikushch@gmail.com>
2024-01-22 15:29:55 +0300

    feat: add functionality for 06-build-page - create common function for build page, merge styles

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -5,14 +5,13 @@ const path = require('path');
 const distDir = path.join(__dirname, 'project-dist');
 const templateFile = path.join(__dirname, 'template.html');
 const componentsPath = path.join(__dirname, 'components');
+const stylesPath = path.join(__dirname, 'styles');
+const distStyles = path.join(distDir, 'style.css');
 
-fs.mkdir(
-  distDir,
-  { recursive: true },
-  (err) => {
-    if (err) throw err;
-  }
-);
+async function createDistFolder(dist) {
+  await fsPromises.rm(dist, { recursive: true, force: true });
+  await fsPromises.mkdir(dist, { recursive: true });
+}
 
 async function createHtml(template, components, dist) {
   try {
@@ -50,4 +49,34 @@ async function createHtml(template, components, dist) {
   }
 }
 
-createHtml(templateFile, componentsPath, distDir);
+async function bundleStyles(sourcePath, distFile) {
+  try {
+    const files = await fsPromises.readdir(sourcePath, { withFileTypes: true });
+    const stylesArr = [];
+    for (const file of files) {
+      if (file.isFile()) {
+        const { ext } = path.parse(file.name);
+        if (ext === '.css') {
+          const data = await fsPromises.readFile(
+            path.resolve(file.path, file.name),
+            { encoding: 'utf8' }
+          );
+          stylesArr.push(data);
+        }
+      }
+    }
+
+    const writableStream = fs.createWriteStream(distFile);
+    writableStream.write(stylesArr.join(''));
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+async function buidPage(dist, template, components, styles, distStyle) {
+  await createDistFolder(dist);
+  await createHtml(template, components, dist);
+  bundleStyles(styles, distStyle)
+}
+
+buidPage(distDir, templateFile, componentsPath, stylesPath, distStyles);
